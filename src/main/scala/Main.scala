75e99ce099d768b9daed025caf06854825d90f48
lucawperri <lucawperri@gmail.com>
2024-11-25 21:48:43 -0800

    Initial K-Nearest Neighbors Implementation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.SparkContext._
 import scala.io._
-import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -13,11 +13,22 @@ object Main {
     Logger.getLogger("akka").setLevel(Level.OFF)
     System.setProperty("hadoop.home.dir", "C:/winutils/")
 
-    loadAndNormalizeData()
+    val data = loadAndNormalizeData()
+
+    // Split the dataset
+    val Array(trainingData, validationData, testData) = data.randomSplit(Array(0.6, 0.2, 0.2), seed = 42)
+
+    // Tune k using the validation set
+    val kValues = Seq(3, 5, 7, 9)
+    val bestK = tuneK(trainingData, validationData, kValues)
+    println(s"Best k value: $bestK")
+
+    // Evaluate the model on the test set
+    val testAccuracy = evaluateModel(trainingData, testData, bestK)
+    println(f"Test Accuracy: $testAccuracy%.2f%%")
   }
 
-  private def loadAndNormalizeData(): Unit = {
-
+  private def loadAndNormalizeData(): RDD[(String, Int, Array[Double])] = {
     val conf = new SparkConf()
       .setAppName("Cancer KNN")
       .setMaster("local[*]")
@@ -46,18 +57,63 @@ object Main {
     val featureMin = features.reduce((a, b) => a.zip(b).map { case (x, y) => math.min(x, y) })
     val featureMax = features.reduce((a, b) => a.zip(b).map { case (x, y) => math.max(x, y) })
 
-    // Normalize features using Min-Max scaling (lessens the impact of outliers on data)
-    val normalizedData = parsedData.map { case (id, diagnosis, features) =>
+    // Normalize features using Min-Max scaling
+    parsedData.map { case (id, diagnosis, features) =>
       val normalizedFeatures = features.zip(featureMin.zip(featureMax)).map {
         case (value, (min, max)) =>
           if (max != min) (value - min) / (max - min) else 0.0
       }
       (id, diagnosis, normalizedFeatures)
     }
+  }
 
-    //normalized features in order:
-    normalizedData.take(5).foreach { case (id, diagnosis, features) =>
-      println(s"ID: $id, Diagnosis: $diagnosis, Normalized Features: ${features.mkString(", ")}")
+  private def classifyKNN(trainingData: RDD[(String, Int, Array[Double])], testData: RDD[(String, Int, Array[Double])], k: Int): RDD[(String, Int)] = {
+    // Pair each test point with all training points (Cartesian product)
+    val cartesianData = testData.cartesian(trainingData)
+
+    // Compute distances for all pairs
+    val distances = cartesianData.map { case ((testId, _, testFeatures), (_, trainingLabel, trainingFeatures)) =>
+      val distance = math.sqrt(testFeatures.zip(trainingFeatures).map {
+        case (a, b) => math.pow(a - b, 2)
+      }.sum)
+      (testId, (distance, trainingLabel))
     }
+
+    // Group by test point and find k-nearest neighbors
+    val kNearestNeighbors = distances.groupByKey().mapValues { neighbors =>
+      neighbors.toSeq.sortBy(_._1).take(k) // Sort by distance and take the k nearest
+    }
+
+    // Predict the majority class for each test point
+    kNearestNeighbors.mapValues { neighbors =>
+      neighbors.groupBy(_._2) // Group by label
+        .mapValues(_.size) // Count occurrences of each label
+        .maxBy(_._2) // Select the label with the maximum count
+        ._1
+    }
+  }
+
+  private def tuneK(trainingData: RDD[(String, Int, Array[Double])], validationData: RDD[(String, Int, Array[Double])], kValues: Seq[Int]): Int = {
+    val accuracyPerK = kValues.map { k =>
+      val accuracy = evaluateModel(trainingData, validationData, k)
+      (k, accuracy)
+    }
+    val bestK = accuracyPerK.maxBy(_._2)._1
+    bestK
+  }
+
+  private def evaluateModel(trainingData: RDD[(String, Int, Array[Double])], testData: RDD[(String, Int, Array[Double])], k: Int): Double = {
+    // Predict labels for the test data
+    val predictions = classifyKNN(trainingData, testData, k)
+
+    // Join predictions with actual labels
+    val joinedData = testData.map { case (id, actual, _) => (id, actual) }.join(predictions)
+
+    // Compute accuracy
+    val correctPredictions = joinedData.filter { case (_, (actual, predicted)) => actual == predicted }.count()
+    val totalPredictions = testData.count()
+
+    val accuracy = (correctPredictions.toDouble / totalPredictions) * 100
+    accuracy
   }
 }
